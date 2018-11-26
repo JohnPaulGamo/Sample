cd78f9b45d0d275c40ea5756d83cf2bbbcc319ec
JohnPaulGamo <45374636+JohnPaulGamo@users.noreply.github.com>
2018-11-26 15:52:43 -0800

    Create sample_Gamo.py

---
+++ sample_Gamo.py
@@ -0,0 +1,42 @@
+
+# Declaring and initializing variables in Python
+
+#Strings
+Red = "Apple"
+Yellow = "Mango"
+Violet = "Grapes"
+Green = "Dalandan"
+
+#Numerical Values
+bilang_ng_prutas = 10
+bilangNgPrutas = 30
+kabusugan = 30
+
+print(Red)
+print(Yellow)
+print(Violet)
+print(Green)
+
+print(bilang_ng_prutas)
+print(bilangNgPrutas)
+print(kabusugan)
+
+# Arithmetic
+addition = 0 + 0
+subtraction = 1 - 1
+division = 1 / 1
+multiplication = 3 * 1
+modulus = 0 & 0
+
+print(addition)
+print(subtraction)
+print(division)
+print(multiplication)
+print(modulus)
+
+if addition > division:
+    print ("Hello")
+elif modulus > multiplication:
+    print("Kowloon")
+else:
+    print ("Hi")
